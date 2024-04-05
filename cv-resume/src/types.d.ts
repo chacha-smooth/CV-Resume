0bdc65532a6c0a3d16238cb9d7b511e2a6b879fd
Ingrid <109824614+chacha-smooth@users.noreply.github.com>
2024-04-05 15:58:31 -0400

    Update types.d.ts

--- cv-resume/src/types.d.ts
+++ cv-resume/src/types.d.ts
@@ -7,10 +7,15 @@ export type yourDetail ={
     phone: string,
     address: string
   }
-
+  
+export type skill = {
+  skillList:string
+}
 export type yourResume ={
     yourDetail: yourDetail
     profileSummary:string
+    experienceArray: experienceInput[],
+    skill:skill
   }
 
 
@@ -20,4 +25,12 @@ export type experienceInput = {
     experienceDate:string,
     experienceCompany: string,
     experienceDescription:string
+}
+
+
+export type educationInput = {
+  id:number,
+  educationDegree: string, 
+  educationDate:string,
+  educationInstitution: string,
 }
